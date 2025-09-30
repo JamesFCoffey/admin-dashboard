a65cf7bf6ab4c23eb4a0c25a25836d154137f8fa
James Coffey <james.francis.coffey@gmail.com>
2025-09-30 15:51:30 -0500

    Change icon

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <ThemedLayoutV2
       Header={Header}
-      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Refine" />}
+      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="" icon={<img src="/brand-mark.svg" alt="Dunder Mifflin logo" style={{ width: 48, height: 48 }} />} />}
     >
       {children}
     </ThemedLayoutV2>
