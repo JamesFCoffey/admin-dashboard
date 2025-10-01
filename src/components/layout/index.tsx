871078a07c3f4d0f7f8d7ca9eec112c4db42a365
James Coffey <james.francis.coffey@gmail.com>
2025-09-30 21:22:52 -0500

    Use <Image /> from next/image to automatically optimize logo

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,5 @@
 import { ThemedLayoutV2, ThemedTitleV2 } from '@refinedev/antd';
+import Image from 'next/image';
 import Header from './header';
 import React from 'react';
 
@@ -6,7 +7,21 @@ const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <ThemedLayoutV2
       Header={Header}
-      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="" icon={<img src="/brand-mark.svg" alt="Dunder Mifflin logo" style={{ width: 48, height: 48 }} />} />}
+      Title={(titleProps) => (
+        <ThemedTitleV2
+          {...titleProps}
+          text=""
+          icon={
+            <Image
+              src="/brand-mark.svg"
+              alt="Dunder Mifflin logo"
+              width={48}
+              height={48}
+              priority
+            />
+          }
+        />
+      )}
     >
       {children}
     </ThemedLayoutV2>
